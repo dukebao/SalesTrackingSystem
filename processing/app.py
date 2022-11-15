f29bbdc4ffe7fa92a1c13f4061bdcb036f8eb278
dukebao <78891975+dukebao@users.noreply.github.com>
2022-11-14 22:13:19 -0800

    updated processing app

--- processing/app.py
+++ processing/app.py
@@ -247,5 +247,5 @@ CORS(app.app)
 app.app.config['CORS_HEADERS'] = 'Content-Type'
 
 if __name__ == '__main__':
-    # init_scheduler()
+    init_scheduler()
     app.run(port=8100, use_reloader=False)
